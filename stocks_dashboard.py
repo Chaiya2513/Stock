58e10011febd104af63846d72a2a061817a7b3b4
Chaiya2513 <45156571+Chaiya2513@users.noreply.github.com>
2026-03-15 23:39:30 +0700

    Update stocks_dashboard.py

--- stocks_dashboard.py
+++ stocks_dashboard.py
@@ -138,7 +138,7 @@ for symbol in stock_symbols:
     real_time_data = fetch_stock_data(symbol, '1d', '1m')
     if not real_time_data.empty:
         real_time_data = process_data(real_time_data)
-        last_price = real_time_data['Close'].iloc[-1]
+        last_price = float(real_time_data['Close'].iloc[-1])
         change = last_price - real_time_data['Open'].iloc[0]
         pct_change = (change / real_time_data['Open'].iloc[0]) * 100
         st.sidebar.metric(f"{symbol}", f"{last_price:.2f} USD", f"{change:.2f} ({pct_change:.2f}%)")
